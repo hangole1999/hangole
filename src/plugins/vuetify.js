4fd48f59ff7a568f6f09f039becdf4d521df08a6
hangole1999 <hangole1999@gmail.com>
2020-06-26 13:20:53 +0900

    Fix default font family

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,35 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+const vuetify = new Vuetify({
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        tertiary: colors.pink.base,
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      },
+      light: {
+        primary: '#1976D2',
+        secondary: '#424242',
+        tertiary: colors.pink.base,
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  }
+});
+
+export default vuetify;
